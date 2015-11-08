bf9b0ab8295391e862dc62b18efab20084bdead4
Andy <andy9775@gmail.com>
2015-11-07 21:11:40 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# React-Native-CircularProgress
-A pure React Native Component for circular progress bars
+## React-Native-CircularProgress
+A pure React Native Component for circular progress bars.
+
+## Demo
+
+<a href="https://github.com/andy9775/React-Native-CircularProgress/master/demo.gif"><img src="https://github.com/andy9775/React-Native-CircularProgress/master/demo.gif" width="350"></a>
+
+## Basic Usage
+
+```javascript
+
+'use strict';
+var React = require('react-native');
+var {View, Text} = React;
+
+module.exports = React.createClass({
+  getInitialState(){
+
+    return { progress:0};
+  },
+  componentDidMount(){
+    // automatically increment the progress
+    var time = 150;
+    var count = 0;
+    setInterval(() => {
+      if (++count < 15){
+        return;
+      }
+      var progress = this.state.progress + Math.round((Math.random() * 4));
+      if (progress >100) {
+        progress = 0;
+        count = 0;
+      }
+      this.setState({progress: progress});
+    }, time);
+  },
+  render() {
+    var progress = this.state.progress;
+    // displayed inside of the component
+    var innerDisplay = (
+      <View style={{width: 200, height: 200, flex:1, justifyContent: 'center',
+      alignItems: 'center', backgroundColor: '#036282'}}>
+        <Text style={{fontSize: 30}}>{progress + "%"}</Text>
+      </View>
+    );
+
+    return (<View style={{backgroundColor: 'orange',flex:1, flexDirection: 'row'}}>
+      <View style={{ flex: 1, top:200, left: 20}}>
+        <CircularProgressDisplay.Hollow size={200}  progressBarWidth={20} outlineWidth={0} outlineColor={'black'}
+                                        backgroundColor={'orange'} progressBarColor={'#02BAF7'} innerComponent={innerDisplay}
+                                        rotate={((progress/100)*360)}/>
+      </View>
+    </View>);
+
+  }
+});
+```
+
+## Hollow Progress Bar Props
+- **`size`** _(Number:ReactComponent)_ - The size of the component. The resulting components height and width
+are equal
+- **`progressBarWidth`** _(Integer)_ - The width of the circular progress bar.
+- **`progressBarColor`** _(String)_ - The color of the progress bar.
+- **`backgroundColor`** _(String)_ - The color of the center of the component and the zero progress area.
+- **`outlineWidth`** _(Integer)_ - If this prop is present, the resulting component has a round border of this width.
+- **`outlineColor`** _(String)_ - The color of the border outlining the component.
+- **`rotate`** _(Integer)_ - The current progress to track specified in degrees. E.G. 270. Can take a value between 0
+and 360, inclusive.
+- **`innerComponent`** _(ReactComponent)_ - A component to display inside of the round hollow progress bar. Can be used
+display an inner progress bar, or track the current progress with a `<Text>` component.
+
+## Filled Progress Bar Props
+- **`size`** _(Number:ReactComponent)_ - The size of the component. The resulting components height and width
+are equal
+- **`rotate`** _(Integer)_ - The current progress to track specified in degrees. E.G. 270. Can take a value between 0
+and 360, inclusive.
+- **`backgroundColor`** _(String)_ - The color of the center of the component and the zero progress area. This is the color
+of the component when no progress has been made.
+- **`progressBarColor`** _(String)_ - The color of the progress bar. This is the color when progress has been made.
+
+---
+
+**MIT Licensed**
---
+++ index.js
@@ -0,0 +1,169 @@
+/*
+ * Coded by: Andy (github.com/andy9775)
+ */
+'use strict';
+
+var React = require('react-native');
+
+var {View} = React;
+
+var Filled = React.createClass({
+  propTypes: {
+    size: React.PropTypes.number,
+    rotate: React.PropTypes.number,
+    componentColor: React.PropTypes.string,
+    progressColor: React.PropTypes.string
+  },
+
+  render() {
+    var size = this.props.size,
+      componentColor = this.props.componentColor,
+      progressBarColor = this.props.progressBarColor,
+      leftProgressBarRotate = 0, rightProgressBarRotate = 0,
+      rotate = this.props.rotate >= 360 ? 360 : this.props.rotate,
+      rightProgressBarColor = this.props.progressBarColor;
+
+    if (rotate < 180) {
+      rightProgressBarRotate = rotate;
+      leftProgressBarRotate = 0;
+    } else {
+      rightProgressBarRotate = 0;
+      leftProgressBarRotate = rotate - 180;
+      rightProgressBarColor = progressBarColor;
+    }
+
+
+    var leftProgressDisplay = (
+      <View
+        style={{backgroundColor: 'transparent',position: 'absolute', width: size, height: size,
+        transform: [{rotate: leftProgressBarRotate + 'deg'}]}}>
+        <View style={{position: 'absolute', flex:1, width: size/2, height: size,
+        borderTopLeftRadius: size/2, borderBottomLeftRadius: size/2, backgroundColor: componentColor}}/>
+        <View style={{position: 'absolute', flex:1, width: size/2, height: size, left: size/2,
+        borderTopRightRadius: size/2, borderBottomRightRadius: size/2,backgroundColor: progressBarColor}}/>
+      </View>
+    );
+
+
+    var rightProgressDisplay = (
+      <View style={{backgroundColor: 'transparent', position: 'absolute', width: size, height: size,
+        transform: [{rotate: rightProgressBarRotate + 'deg'}]}}>
+        <View style={{position: 'absolute', flex:1, width: size/2, height: size,
+        borderTopLeftRadius: size/2, borderBottomLeftRadius: size/2, backgroundColor: 'transparent'}}/>
+
+        {rotate < 180 ? (<View style={{position: 'absolute', flex:1, width: size, height: size, left: size/2,
+          borderTopRightRadius: size/2, borderBottomRightRadius: size/2, backgroundColor: componentColor}}/>) :
+          (<View style={{position: 'absolute', flex:1, width: size/2, height: size ,
+          left: size/2, backgroundColor: progressBarColor,
+          borderBottomRightRadius: size/2, borderTopRightRadius: size/2}}/>
+          )}
+      </View>);
+
+    return (
+      <View style={{width: size, height: size, overflow:1, borderRadius: size/2}}>
+        {leftProgressDisplay}
+        {rightProgressDisplay}
+      </View>
+    );
+  }
+});
+
+var Hollow = React.createClass({
+  propTypes: {
+    size: React.PropTypes.number,
+    progressBarWidth: React.PropTypes.number,
+    backgroundColor: React.PropTypes.string,
+    progressBarColor: React.PropTypes.string,
+    outlineWidth: React.PropTypes.number,
+    outlineColor: React.PropTypes.string,
+    rotate: React.PropTypes.number,
+    innerComponent: React.PropTypes.component
+  },
+  render(){
+    var rotateValue = this.props.rotate >= 360 ? 360 : this.props.rotate;
+
+    var size = this.props.size,
+      progressBarWidth = this.props.progressBarWidth,
+      backgroundColor = this.props.backgroundColor,
+      progressBarColor = this.props.progressBarColor,
+      outlineWidth = this.props.outlineWidth || 0,
+      outlineColor = this.props.outlineColor || 'transparent',
+      leftRotate = 0, rightRotate = 0;
+
+    if (rotateValue < 180) {
+      rightRotate = rotateValue;
+    } else {
+      leftRotate = rotateValue - 180;
+    }
+    var leftProgressBar = (
+      <View style={{width: size, height: size, backgroundColor: 'transparent', position: 'absolute'}}>
+        <View style={{width: size/2, height: size, position: 'absolute',
+        borderBottomLeftRadius: size/2, borderTopLeftRadius: size/2,
+        borderRightWidth: 0,backgroundColor: backgroundColor,
+        borderTopWidth: progressBarWidth, borderBottomWidth: progressBarWidth, borderLeftWidth: progressBarWidth,
+        borderLeftColor: progressBarColor, borderTopColor: progressBarColor, borderBottomColor: progressBarColor}}/>
+
+        <View style={{width: size/2, height: size, left: size/2,
+        position: 'absolute', backgroundColor: backgroundColor}}/>
+      </View>
+
+    );
+
+    var rightProgressBar = (
+      <View style={{width: size, height: size, backgroundColor: 'transparent', position: 'absolute'}}>
+        <View style={{width: size/2, height: size, position: 'absolute', backgroundColor: 'transparent'}}/>
+
+        <View style={{width: size/2, height: size, position: 'absolute', left: size/2,
+        borderBottomRightRadius: size/2, borderTopRightRadius: size/2,
+        borderLeftWidth: 0, backgroundColor: backgroundColor,
+        borderTopWidth: progressBarWidth, borderBottomWidth: progressBarWidth, borderRightWidth: progressBarWidth,
+        borderRightColor: progressBarColor, borderTopColor: progressBarColor, borderBottomColor: progressBarColor}}/>
+      </View>
+    );
+
+    var leftProgressOverlay = (
+      <View style={{width: size, height: size,position: 'absolute', backgroundColor: 'transparent',
+        transform: [{rotate: leftRotate + 'deg'}]}}>
+        <View style={{width: size/2, height: size, position: 'absolute', backgroundColor: backgroundColor}}/>
+        <View
+          style={{width: size/2, height: size, position: 'absolute', backgroundColor: 'transparent', left: size/2}}/>
+      </View>
+    );
+
+    var rightProgressOverlay = (
+      <View style={{width: size, height: size, position: 'absolute', backgroundColor: 'transparent',
+        transform: [{rotate: rightRotate + 'deg'}]}}>
+        <View style={{width: size/2, height: size, position: 'absolute', backgroundColor: 'transparent'}}/>
+        <View
+          style={{width: size/2, height: size, position: 'absolute', backgroundColor: backgroundColor, left: size/2}}/>
+      </View>
+    );
+
+    var innerView = this.props.innerComponent ? (
+      <View style={{width: size - progressBarWidth*2, height: size - progressBarWidth*2,
+        borderRadius: (size - progressBarWidth*2)/2,
+        overflow:1, backgroundColor: 'transparent',
+        position: 'absolute', left: progressBarWidth, top: progressBarWidth,
+        justifyContent: 'center', alignItems:'center'}}>
+        {this.props.innerComponent}
+      </View>
+    ) : (<View/>);
+
+    var views;
+    if (rotateValue < 180) {
+      views = [leftProgressBar, leftProgressOverlay, rightProgressBar, rightProgressOverlay];
+    } else {
+      views = [leftProgressBar, leftProgressOverlay, rightProgressBar];
+    }
+    return (
+      <View
+        style={{width: size + (outlineWidth * 2), height: size + (outlineWidth * 2), overflow:1,
+        borderRadius: (size + (outlineWidth * 2))/2, borderWidth: outlineWidth, borderColor: outlineColor}}>
+        {views}
+        {innerView}
+      </View>
+    );
+  }
+});
+
+module.exports = {Hollow, Filled};
